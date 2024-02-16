dbe9fb0c01dbb123d3eb7f801a7dd1147954d93b
Thaddeus <thaddeusleezx@gmail.com>
2024-02-17 02:03:30 +0800

    chore: init nx express app

---
+++ backend/src/main.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import cors from 'cors';
+import * as bodyParser from 'body-parser';
+import HttpException from './app/models/http-exception.model';
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.json({ status: 'API is running on /api' });
+});
+
+/* eslint-disable */
+app.use(
+  (
+    err: Error | HttpException,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+  ) => {
+    // @ts-ignore
+    if (err && err.name === 'UnauthorizedError') {
+      return res.status(401).json({
+        status: 'error',
+        message: 'missing authorization credentials',
+      });
+      // @ts-ignore
+    } else if (err && err.errorCode) {
+      // @ts-ignore
+      res.status(err.errorCode).json(err.message);
+    } else if (err) {
+      res.status(500).json(err.message);
+    }
+  },
+);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.info(`server up on port ${PORT}`);
+});
